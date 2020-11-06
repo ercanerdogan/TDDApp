acbf17f2ef4176faf99e975e0608e0d38db8dae7
ercanerdogan@gmail.com <ercanerdogan@gmail.com>
2020-11-06 04:04:00 +0300

    first test example method was added

---
+++ UnitTest.APP/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTest.APP
+{
+    public class Calculator
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ UnitTest.Test/CalculatorTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnitTest.APP;
+using Xunit;
+
+namespace UnitTest.Test
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void AddTest()
+        {
+            int a = 5;
+            int b = 20;
+            var calculator = new Calculator();
+
+            var total = calculator.add(a, b);
+
+            Assert.Equal<int>(25, total);
+
+        }
+
+
+
+    }
+}
