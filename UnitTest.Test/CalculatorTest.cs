1432f933d534f65e3304dedae17b095565c4d5d6
ercanerdogan@gmail.com <ercanerdogan@gmail.com>
2020-11-08 06:02:58 +0300

    -Assert methods were implemented -test method was added which is testing with given paremeters

--- UnitTest.Test/CalculatorTest.cs
+++ UnitTest.Test/CalculatorTest.cs
@@ -22,6 +22,55 @@ namespace UnitTest.Test
         }
 
 
+        [Fact]
+        public void TestContain()
+        {
+            var names = new List<string>() { "Turkey", "ABD", "Germany", "Holland", "UK" };
+
+            Assert.Contains(names, x => x == "UK");
+        }
+
+        [Fact]
+        public void TestStartWith()
+        {
+            Assert.StartsWith("Ercan", "Ercan Erdoğan");
+        }
+
+        [Fact]
+        public void TestEndWith()
+        {
+            Assert.StartsWith("Ercan", "Ercan Erdoğan");
+        }
+
+        [Fact]
+        public void TestRange()
+        {
+
+            Assert.InRange(15, 1, 100);
+
+        }
+
+        [Fact]
+        public void TestType()
+        {
+
+            Assert.IsType<string>("Ercan");
+
+        }
+
+        [Theory]
+        [InlineData(5, 20, 25)]
+        [InlineData(3, 5, 8)]
+        public void TestSumWithParameter(int a, int b, int expectedTotal)
+        {
+            var calculator = new Calculator();
+
+            var actualTotal = calculator.add(a, b);
+
+            Assert.Equal(expectedTotal, actualTotal);
+        }
+
+
 
     }
 }
